382e1eec429a0f9e7f18c464b96868af3435580b
DestinyJie <2398329445@qq.com>
2021-03-23 00:13:53 +0800

    测试

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -14,7 +14,8 @@
                 <nuxt-link to="/">首页</nuxt-link>
                 <nuxt-link to="/post">旅游攻略</nuxt-link>
                 <nuxt-link to="/hotel">酒店</nuxt-link>
-                <nuxt-link to="/air">国内机票</nuxt-link>  
+                <nuxt-link to="/air">国内机票</nuxt-link> 
+                   <nuxt-link to="/air">国外机票</nuxt-link> 
             </el-row>
 
             <!-- 登录/用户信息 -->
